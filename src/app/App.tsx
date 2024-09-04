1a4d98a52777d48b598eec0339433b888c5b2e2b
Andrey_Marusov <marusov140@gmail.com>
2024-09-04 14:24:23 +0300

    init

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import tg from './telegram_logo.svg';
 import './App.css';
 
 function App() {
@@ -8,8 +9,25 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          {' '}
+          Приветствую! Меня зовут Марусов Андрей, и я Junior разработчик. В мои руки уже попали React, различные
+          сборщики и state менеджеры, и я с удовольствием работаю с этими инструментами.{' '}
+        </p>{' '}
+        <p>
+          {' '}
+          На этом курсе мне хочется укрепить свои знания, освоить новые технологии и, самое главное, начать глубже
+          понимать, как всё это работает изнутри.{' '}
+        </p>{' '}
+        <p>
+          После завершения курса я надеюсь на новый виток в своей карьере, будь то повышение на текущем месте или поиск
+          более интересной и высокооплачиваемой работы.
         </p>
+        <div className="App-tg">
+          Контакты:
+          <a href="https://t.me/marusov_aa" target="_blank" rel="noopener noreferrer">
+            <img src={tg} className="App-tg-logo" alt="tg_logo" />
+          </a>
+        </div>
       </header>
     </div>
   );
